ecb82534b08b35a4fa2e5b4ff4c55c4251b3db7a
tinoest <tinoest@gmail.com>
2018-04-28 13:41:52 +0100

    Changes to start to style the front page

--- ElkBlog.controller.php
+++ ElkBlog.controller.php
@@ -22,8 +22,47 @@ class ElkBlog_Controller extends Action_Controller implements Frontpage_Interfac
 	public function action_elkblog()
 	{
 		global $context;
+
 		$context['page_title']		= 'Elk Blog';
 		$context['sub_template'] 	= 'elkblog';
+		
+		$db 		= database();
+		$request 	= $db->query('', '
+			SELECT id, name
+			FROM {db_prefix}blog_categories
+			WHERE status = 1'
+		);
+		
+		while ($row = $db->fetch_assoc($request))
+		{
+			$categories[$row['id']] = $row['name'];
+		}
+	
+		$request 	= $db->query('', '
+			SELECT category_id, member_id, dt_published, title, body, views, comments
+			FROM {db_prefix}blog_articles
+			WHERE status = 1'
+		);
+
+		$articles 	= array();
+		while ($row = $db->fetch_assoc($request))
+		{
+			$member	= $db->query('', '
+				SELECT member_name
+				FROM {db_prefix}members
+				WHERE id_member = {int:member_id}',
+				array ( 
+					'member_id' => $row['member_id'],
+				)
+			);
+			$row['member'] 		= $db->fetch_assoc($member)['member_name'];
+			$row['category']	= $categories[$row['category_id']];	
+			$articles[] 		= $row; 
+			
+		}
+
+		$context['articles'] = $articles;
+
 		loadTemplate('ElkBlog');
 	}
 
--- ElkBlog.template.php
+++ ElkBlog.template.php
@@ -4,11 +4,26 @@ function template_elkblog()
 {
 	global $context, $txt;
 	
-	echo '<div id="sp_view_articles">
+	echo '<div id="eb_view_articles">
 		<h3 class="category_header">
 			', $context['page_title'], '
 		</h3>';
 
+	foreach($context['articles'] as $article) {
+
+		echo '<div class="eb_article">';
+		echo '<h4>';
+		echo $article['title'];
+		echo '</h4>';
+		echo '<div class="eb_article_body">';
+		echo $article['body'];
+		echo '</div>';
+		
+		echo sprintf('<div class="eb_article_details">Posted By: %s Category: %s Published %s </div>', $article['member'], $article['category'], $article['dt_published']);
+		echo sprintf('<div class="eb_article_details">Views: %d Comments: %d </div>', $article['views'], $article['comments']);
+
+		echo '</div>';
+	}
 	
 
 
