81e09b56459c64bcfa336a1c990c9a15d5f7752e
tinoest <tinoest@gmail.com>
2018-05-06 14:34:42 +0100

    Renamed ElkBlog

--- themes/default/languages/english/ElkBlog.english.php
+++
@@ -1,6 +0,0 @@
-<?php
-$txt['home_btn'] 				= 'Home';
-$txt['elkblog-admin'] 				= 'ElkBlog';
-$txt['elkblog-adminConfiguration'] 		= 'Admin ElkBlog';
-$txt['elkblog-addarticle']			= 'ElkBlog Add Article'; 
-$txt['elkblog-listarticle'] 			= 'ElkBlog List Articles';
