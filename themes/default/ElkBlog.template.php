8390d3b4ad866d193de23cd3f685b6760bcced1c
tinoest <tinoest@gmail.com>
2018-05-06 14:19:38 +0100

    Moved text around on articles

--- themes/default/ElkBlog.template.php
+++ themes/default/ElkBlog.template.php
@@ -11,14 +11,12 @@ function template_elkblog()
 		echo '<div class="eb_article" style="padding: 0.1em;">';
 		echo '<h3 class="category_header">';
 		echo $article['title'];
-		echo sprintf('<span class="views_text"> (Views: %d, Comments: %d) </span>', $article['views'], $article['comments']);
 		echo '</h3>';
+		echo sprintf('<span class="views_text"> Views: %d | Comments: %d </span>', $article['views'], $article['comments']);
+		echo sprintf('<span class="views_text"> | Written By: %s in %s | %s </span>', $article['member'], $article['category'], htmlTime($article['dt_published']));
 		echo '<section>';
 		echo '<article class="post_wrapper forumposts">';
 		echo $article['body'];
-		echo '<header class="keyinfo">';	
-		echo sprintf('Posted By: %s<br />Category: %s<br />Published: %s', $article['member'], $article['category'], htmlTime($article['dt_published']));
-		echo '</header>';
 		echo '</article>';
 		echo '</section>';
 		echo '</div>';
