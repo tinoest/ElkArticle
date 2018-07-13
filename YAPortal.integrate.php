f37c5e3bf9c7b7d90abbf59df1a5656fb2ad08e8
tino <tinoest@gmail.com>
2018-07-13 15:43:07 +0100

    Changed gallery image a photo icon

--- YAPortal.integrate.php
+++ YAPortal.integrate.php
@@ -134,11 +134,19 @@ class YAPortal
         // Show the Gallery Option Tab
         if(!empty($modSettings['yaportal-gallery-menu-item'])) {
 			loadLanguage('YAPortal');
+
+            $context['html_headers'] .= '
+            <style>
+                .i-gallery::before {
+                    content: url("data:image/svg+xml,%3Csvg version=\'1.1\' xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 32 32\'%3E%3Ctitle%3Ecamera%3C/title%3E%3Cpath d=\'M9.5 19c0 3.59 2.91 6.5 6.5 6.5s6.5-2.91 6.5-6.5-2.91-6.5-6.5-6.5-6.5 2.91-6.5 6.5zM30 8h-7c-0.5-2-1-4-3-4h-8c-2 0-2.5 2-3 4h-7c-1.1 0-2 0.9-2 2v18c0 1.1 0.9 2 2 2h28c1.1 0 2-0.9 2-2v-18c0-1.1-0.9-2-2-2zM16 27.875c-4.902 0-8.875-3.973-8.875-8.875s3.973-8.875 8.875-8.875c4.902 0 8.875 3.973 8.875 8.875s-3.973 8.875-8.875 8.875zM30 14h-4v-2h4v2z\'%3E%3C/path%3E%3C/svg%3E%0A");
+                }
+            </style>';
+
 			$buttons = elk_array_insert($buttons, 'home', array (
 				'gallery' => array(
 					'title' 	    => $txt['gallery_btn'],
 					'href' 		    => $scripturl . '?action=gallery',
-					'data-icon'     => 'i-home',
+					'data-icon'     => 'i-gallery',
 					'show'          => true,
 					'action_hook' 	=> true,
 	            ),
